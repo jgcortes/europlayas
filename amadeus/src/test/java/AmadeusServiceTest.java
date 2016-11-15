a6d7d7e8b75d36947d2ac3be76e05061a617fa3f
gonzalo cortes <gcortes@travelcompositor.com>
2016-11-15 16:54:38 +0100

    cambios

--- amadeus/src/test/java/AmadeusServiceTest.java
+++ amadeus/src/test/java/AmadeusServiceTest.java
@@ -4,27 +4,31 @@
 
 import com.europlayas.amadeus.bindings.*;
 import com.europlayas.amadeus.engine.AmadeusService;
+
+import com.europlayas.amadeus.util.GzipUtil;
+import com.europlayas.amadeus.util.SerializerUtil;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.IOUtils;
 import org.junit.*;
 import org.junit.runner.*;
-import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
 
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import static org.hamcrest.core.Is.is;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.when;
 
+import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.datatype.DatatypeFactory;
-import javax.xml.datatype.XMLGregorianCalendar;
-import javax.xml.ws.BindingProvider;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
-import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AmadeusServiceTest {
@@ -32,6 +36,8 @@ public class AmadeusServiceTest {
     private static String ENDPOINT = "https://demo.amadeus.fr/air-5/ws/airService";
     private static String USER = "to_Europlayas_Test";
     private static String PASSWORD = "HKPo8i9n";
+
+    private static JAXBContext JAXB_CONTEXT = null;
 
     @Before
     public void setUp() {
@@ -54,9 +60,22 @@ public class AmadeusServiceTest {
 
     }
 
+    @Test
+    public void doValidate_TestSerialization() throws JAXBException {
+        try{
+            serializeProviderReferece();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
 
     private  LowFareSearchRequest createLowFareRequest() throws Exception{
         LowFareSearchRequest lowFareSearchRequest = new LowFareSearchRequest();
+
+        LowFareSearchRequest.SearchPreferences searchPreferences = new LowFareSearchRequest.SearchPreferences();
+        searchPreferences.setReturnAdditionalServices(true);
+        lowFareSearchRequest.setSearchPreferences(searchPreferences);
 
         LowFareSearchRequest.Journey journey = new LowFareSearchRequest.Journey();
         LowFareSearchRequest.Journey.OriginDestinations originDestinations = new LowFareSearchRequest.Journey.OriginDestinations();
@@ -72,11 +91,25 @@ public class AmadeusServiceTest {
 
 
         SeatedPassengerList seatedPassengerList = new SeatedPassengerList();
+
+        InfantHoldingSeatedPassenger setatPassangerWithInfant = new InfantHoldingSeatedPassenger();
+        setatPassangerWithInfant.setId(1);
+        setatPassangerWithInfant.setPassengerType(InfantHoldingAllowedPassengerType.ADULT);
+        HeldInfantPassenger heldInfantPassenger = new HeldInfantPassenger();
+        heldInfantPassenger.setId(2);
+        setatPassangerWithInfant.setHeldInfant( heldInfantPassenger );
+
+        seatedPassengerList.getPassengers().add( setatPassangerWithInfant );
+        lowFareSearchRequest.setPassengers(seatedPassengerList);
+
         NonInfantHoldingSeatedPassenger seatedPassenger = new NonInfantHoldingSeatedPassenger();
-        seatedPassenger.setPassengerType(PassengerType.ADULT);
-        seatedPassenger.setId(1);
+        seatedPassenger.setId(3);
+        seatedPassenger.setPassengerType(PassengerType.CHILD);
+
         seatedPassengerList.getPassengers().add(seatedPassenger);
         lowFareSearchRequest.setPassengers(seatedPassengerList);
+
+
 
         SearchPricingMode searchPricingMode = new SearchPricingMode();
         searchPricingMode.setCurrencyCode(CurrencyCode.EUR);
@@ -84,12 +117,22 @@ public class AmadeusServiceTest {
         fareType.setPublishedFares(true);
         fareType.setUnifares(false);
         searchPricingMode.setFareType(fareType);
+
         PassengerFareOptionGroups passengerFareOptionGroups = new PassengerFareOptionGroups();
         PassengerFareOptionList passengerFareOptionList = new PassengerFareOptionList();
         PassengerFareOption passengerFareOption = new PassengerFareOption();
         passengerFareOption.setPassengerId(1);
         passengerFareOption.setFareOption(FareOption.STANDARD);
         passengerFareOptionList.getPassengerFareOptions().add(passengerFareOption);
+         passengerFareOption = new PassengerFareOption();
+        passengerFareOption.setPassengerId(2);
+        passengerFareOption.setFareOption(FareOption.STANDARD);
+        passengerFareOptionList.getPassengerFareOptions().add(passengerFareOption);
+        passengerFareOption = new PassengerFareOption();
+        passengerFareOption.setPassengerId(3);
+        passengerFareOption.setFareOption(FareOption.STANDARD);
+        passengerFareOptionList.getPassengerFareOptions().add(passengerFareOption);
+
         passengerFareOptionGroups.getPassengerFareOptionGroups().add(passengerFareOptionList);
         searchPricingMode.setPassengerFareOptionGroups(passengerFareOptionGroups);
         lowFareSearchRequest.setPricing(searchPricingMode);
@@ -131,5 +174,39 @@ public class AmadeusServiceTest {
     }
 
 
+    public void serializeProviderReferece(){
+        try{
+            BookRequest bookRequest = new  BookRequest();
+            com.europlayas.amadeus.bindings.Journey journey = new com.europlayas.amadeus.bindings.Journey();
+            Journey.Itineraries itineraries = new Journey.Itineraries();
+            Itinerary itinerary = new Itinerary();
+            itinerary.setId(1);
+            itineraries.getItineraries().add(itinerary);
+            journey.setItineraries(itineraries);
+            bookRequest.setJourney(journey);
 
+            BookRequestPricing bookRequestPricing = new BookRequestPricing();
+
+            bookRequest.setPricing(bookRequestPricing);
+            String providerReference = SerializerUtil.seralizeProviderReference(bookRequest);
+            System.out.println("Solution Zip: " + providerReference);
+            bookRequest = SerializerUtil.unSerializeProviderReference (providerReference );
+            System.out.println("Book Solution" + bookRequest.getJourney().getItineraries().getItineraries().get(0).getId());
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+}
